7abc27d4360cfa617bebd7955f7e3ef9a8dfe8f5
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2022-05-23 18:42:01 +1000

    Second commit - import react, bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/StaffComponent";
+import "./App.css";
+import { STAFFS } from "./shared/staffs";
 function App() {
   return (
     <div className="App">
