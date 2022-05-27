e0c2966afe5af8bacc7decc3e53e91f9248efae4
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2022-05-27 17:48:20 +1000

    Add select dish render

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -11,15 +11,37 @@ import {
 class Menu extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      selectedDish: null,
+    };
+  }
+
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+
+  renderDish(dish) {
+    if (dish !== null) {
+      return (
+        <Card>
+          <CardImg width="100%" src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    } else {
+      return <div></div>;
+    }
   }
 
   render() {
     const menu = this.props.dishes.map((dish) => {
       return (
         <div key={dish.id} className="col-12 col-md-5 m-1 ">
-          <Card>
-            <CardImg object src={dish.image} alt={dish.name} />
+          <Card onClick={() => this.onDishSelect(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
             <CardImgOverlay>
               <CardTitle>{dish.name}</CardTitle>
             </CardImgOverlay>
@@ -30,6 +52,7 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
+        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
       </div>
     );
   }
