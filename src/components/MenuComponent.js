92dc2a2780f09106c8602efbe9d2b5c7ffb0087e
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2022-05-28 00:45:09 +1000

    Exercise 1 complete

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+
 import {
   Card,
   CardImg,
@@ -7,6 +8,8 @@ import {
   CardBody,
   CardTitle,
 } from "reactstrap";
+
+import DishDetail from "./DishdetailComponent";
 
 class Menu extends Component {
   constructor(props) {
@@ -18,22 +21,6 @@ class Menu extends Component {
 
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
-  }
-
-  renderDish(dish) {
-    if (dish !== null) {
-      return (
-        <Card>
-          <CardImg width="100%" src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    } else {
-      return <div></div>;
-    }
   }
 
   render() {
@@ -52,9 +39,12 @@ class Menu extends Component {
     return (
       <div className="container">
         <div className="row">{menu}</div>
-        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
+        <DishDetail dish={this.state.selectedDish} />
       </div>
     );
+  }
+  componentDidMount() {
+    console.log("Menu component componentDiMount is invoked");
   }
 }
 
