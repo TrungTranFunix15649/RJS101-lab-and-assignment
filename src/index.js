ad910ded548f56fbd826ef2e0243e8f19cec7042
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2022-05-28 23:06:25 +1000

    lab05.1 completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import "bootstrap-social/bootstrap-social.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+// import registerServiceWorker from ".registerServiceWorker";
 
+ReactDOM.render(<App />, document.getElementById("root"));
+// registerServiceWorker();
+/*
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -16,3 +22,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
