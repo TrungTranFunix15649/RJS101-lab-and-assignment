25b484b9caf6b4b42ecda5b65b5acc5522cd6a3c
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2022-06-06 20:17:03 +1000

    Create login form

--- src/components/StaffComponent.js
+++ src/components/StaffComponent.js
@@ -1,16 +1,21 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 
 import {
   Card,
   CardImg,
-  CardImgOverlay,
-  CardText,
-  CardBody,
   CardTitle,
+  Button,
+  Modal,
+  ModalBody,
+  ModalHeader,
+  Form,
+  FormGroup,
+  Label,
+  Col,
+  Input,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import StaffDetail from "./StaffdetailComponent";
-let search = "";
+
 class StaffList extends Component {
   constructor(props) {
     super(props);
@@ -18,10 +23,21 @@ class StaffList extends Component {
       searchedStaff: "",
       staffs: props.staffs,
       ListForSearch: props.staffs,
+      staffname: "",
+      dob: "",
+      salaryscale: "",
+      startdate: "",
+      department: "",
+      annualleave: "",
+      overtime: "",
+      salary: "",
+      isAddOpen: false,
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.searchName = React.createRef();
     this.handleAdd = this.handleAdd.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleNewStaff = this.handleNewStaff.bind(this);
   }
 
   handleSubmit(event) {
@@ -35,8 +51,22 @@ class StaffList extends Component {
 
     this.setState({ staffs: filteredStaff });
   }
+
+  handleInputChange(event) {
+    const value = event.target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value,
+    });
+  }
+  handleNewStaff(event) {
+    console.log("This state: " + JSON.stringify(this.state));
+    alert("This state: " + JSON.stringify(this.state));
+    this.handleAdd();
+    event.preventDefault();
+  }
   handleAdd() {
-    console.log("Added");
+    this.setState({ isAddOpen: !this.state.isAddOpen });
   }
 
   RenderStaffs(staff) {
@@ -58,17 +88,15 @@ class StaffList extends Component {
             <hr />
           </div>
           <div className="col-xs-2 col-md-1 col-lg-1">
-            <button
-              onClick={this.handleAdd}
-              className="fa fa-plus fa-lg btn-secondary btn-lg btn-block"
-            ></button>
+            <Button outline onClick={this.handleAdd}>
+              <span className="fa fa-plus fa-lg "></span>
+            </Button>
             <hr />
           </div>
           <form
             onSubmit={this.handleSubmit}
             className="  col-12 col-md-6 col-lg-7 float-right"
           >
-            <label htmlFor="search">Tìm nhân viên</label>
             <input
               type="text"
               placeholder="Nhập tên"
@@ -93,6 +121,136 @@ class StaffList extends Component {
             );
           })}
         </div>
+        <Modal isOpen={this.state.isAddOpen} toggle={this.handleAdd}>
+          <ModalHeader toggle={this.handleAdd}>Thêm nhân viên</ModalHeader>
+          <ModalBody>
+            <div className="col-12 col-md-10">
+              <Form onSubmit={this.handleNewStaff}>
+                <FormGroup row>
+                  <Label htmlFor="staffname" md={4}>
+                    Tên:
+                  </Label>
+                  <Col md={8}>
+                    <Input
+                      type="text"
+                      id="staffname"
+                      name="staffname"
+                      placeholder="Họ và tên"
+                      value={this.state.staffname}
+                      onChange={this.handleInputChange}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="dob" md={4}>
+                    Ngày sinh:
+                  </Label>
+                  <Col md={8}>
+                    <Input
+                      type="date"
+                      id="dob"
+                      name="dob"
+                      placeholder="dd/mm/yyyy"
+                      value={this.state.dob}
+                      onChange={this.handleInputChange}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="startdate" md={4}>
+                    Ngày vào công ty:
+                  </Label>
+                  <Col md={8}>
+                    <Input
+                      type="date"
+                      id="startdate"
+                      name="startdate"
+                      placeholder="dd/mm/yyyy"
+                      value={this.state.startdate}
+                      onChange={this.handleInputChange}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="department" md={4}>
+                    Phòng ban:
+                  </Label>
+                  <Col md={8}>
+                    <Input
+                      type="select"
+                      name="department"
+                      value={this.state.department}
+                      onChange={this.handleInputChange}
+                    >
+                      <option>Finance</option>
+                      <option>HR</option>
+                      <option>IT</option>
+                      <option>Marketing</option>
+                      <option>Sale</option>
+                    </Input>
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="salaryscale" md={4}>
+                    Hệ số lương:
+                  </Label>
+                  <Col md={8}>
+                    <Input
+                      type="number"
+                      id="salaryscale"
+                      name="salaryscale"
+                      min={1.0}
+                      step={0.1}
+                      precision={1}
+                      max={3.0}
+                      value={this.state.salaryscale}
+                      onChange={this.handleInputChange}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="annualleave" md={4}>
+                    Số ngày nghỉ còn lại:
+                  </Label>
+                  <Col md={8}>
+                    <Input
+                      type="number"
+                      id="annualleave"
+                      name="annualleave"
+                      min={0.0}
+                      step={0.5}
+                      precision={1}
+                      max={365.0}
+                      value={this.state.annualleave}
+                      onChange={this.handleInputChange}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Label htmlFor="overtime" md={4}>
+                    Số ngày đã làm thêm:
+                  </Label>
+                  <Col md={8}>
+                    <Input
+                      type="number"
+                      id="overtime"
+                      name="overtime"
+                      value={this.state.overtime}
+                      onChange={this.handleInputChange}
+                    />
+                  </Col>
+                </FormGroup>
+                <FormGroup row>
+                  <Col md={{ size: 10, offset: 2 }}>
+                    <Button type="submit" color="primary">
+                      Thêm
+                    </Button>
+                  </Col>
+                </FormGroup>
+              </Form>
+            </div>
+          </ModalBody>
+        </Modal>
       </div>
     );
   }
