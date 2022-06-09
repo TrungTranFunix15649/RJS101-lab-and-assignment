02d0d0a574e87f9008d05b6cbcbe7988b81b6d69
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2022-06-09 22:48:25 +1000

    Lab08.1 complete

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,17 @@
-import { createStore } from "redux"; //allow to create redux store
-import { Reducer, initialState } from "./reducer";
+import { createStore, combineReducers } from "redux"; //allow to create redux store
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
 
 export const ConfigureStore = () => {
-  const store = createStore(Reducer, initialState);
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+    })
+  );
   return store;
 };
