9068eb04bdd56e666ea4ef8fcd8010ce60b5326a
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2022-06-10 16:48:45 +1000

    Lab09.1 redux-thunk complete

--- src/redux/dishes.js
+++ src/redux/dishes.js
@@ -1,7 +1,31 @@
-import { DISHES } from "../shared/dishes";
+// import { DISHES } from "../shared/dishes"; // NO longer needed bcs of thunk
+import * as ActionTypes from "./ActionTypes";
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (
+  state = {
+    isLoading: true, //dishes empty,when obtain dishes from server will be set to false
+    errMess: null, //when dishes failed to be load, it will receive a message from server
+    dishes: [], //need to be loaded from somewhere
+  },
+  action
+) => {
   switch (action.type) {
+    case ActionTypes.ADD_DISHES:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: null,
+        dishes: action.payload,
+      };
+    case ActionTypes.DISHES_LOADING:
+      return { ...state, isLoading: true, errMess: null, dishes: [] };
+    case ActionTypes.DISHES_FAILED:
+      return {
+        ...state,
+        isLoading: false,
+        errMess: action.payload,
+        dishes: [],
+      };
     default:
       return state;
   }
